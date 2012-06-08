ca534c4fe1f22ac8d938e2b0525b83f52271fb76
drupalmk <leisikkeram@gmail.com>
2012-06-08 13:40:13 +0200

    setup category entity

---
+++ src/WSAD/JobsBundle/Entity/Category.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace WSAD\JobsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * WSAD\JobsBundle\Entity\Category
+ */
+class Category
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var string $slug
+     */
+    private $slug;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+}
