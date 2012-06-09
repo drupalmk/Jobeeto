6447e41f0a3b3612bd712ee2a1ab95a9d8f2e473
drupalmk <leisikkeram@gmail.com>
2012-06-09 16:30:19 +0200

    setup jobs fixtures

--- app/autoload.php
+++ app/autoload.php
@@ -17,6 +17,10 @@ $loader->registerNamespaces(array(
 		
 	'Stof'  => __DIR__.'/../vendor/bundles',
 	'Gedmo' => __DIR__.'/../vendor/gedmo-doctrine-extensions/lib',
+		
+	'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
+	'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
+		
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
